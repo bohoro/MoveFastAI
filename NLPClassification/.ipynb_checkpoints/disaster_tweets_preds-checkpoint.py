4071cb6abd7fd64c64dbb5e7db118b695ed907d4
Brian O'Horo <bohoro@devfair0220.h2.fair>
2021-03-08 22:28:01 -0800

    Final fixes for preditions

--- NLPClassification/.ipynb_checkpoints/disaster_tweets_preds-checkpoint.py
+++ NLPClassification/.ipynb_checkpoints/disaster_tweets_preds-checkpoint.py
@@ -1,50 +1,147 @@
-base_dir = "./"
-#!ls "/content/drive/MyDrive/Colab Notebooks/DisasterTweets"
+'''
+disaster_tweets_preds.py is used for evaling the latest and greatet model. The script
+loads the prettained model, evaluates against the training set and the leaked dataset.
+Finally the script predicts agains the kaggle hold out file and creates a submission 
+file submissions.csv.
+'''
 
 import pandas as pd
-#import numpy as np
+import numpy as np
 import torch
+from transformers import DistilBertTokenizerFast, DistilBertForSequenceClassification, Trainer, TrainingArguments
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+base_dir = "./"
+
+### use GPU if avialable ###
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device
+
+# #############################################################################
+# Load the tokenizer and the fine tuned model
+# #############################################################################
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+print()
+print("Loading Fine Tuned Model...")
+model = DistilBertForSequenceClassification.from_pretrained(base_dir + "v1model")
+# set to evaluation rather than training mode
+model.eval() 
+print("Complete")
+print()
+
+# #############################################################################
+# sanity check on the training set
+# #############################################################################
+
+class DisasterDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
+    
+val_dataset=pd.read_csv(base_dir+'train.csv', keep_default_na=False)
+print(val_dataset.head(5))
+val_dataset["fulltext"] = val_dataset["keyword"] + " " + val_dataset["location"] + " " + val_dataset["text"]
+val_dataset_t = val_dataset["fulltext"]
+val_encodings = tokenizer(val_dataset_t.tolist(), truncation=True, padding=True)
+val_dataset = DisasterDataset(val_encodings, val_dataset['target'].tolist())
+
+training_args = TrainingArguments(
+    output_dir='./results',          # output directory
+    num_train_epochs=1,              # total number of training epochs
+    per_device_train_batch_size=16,  # batch size per device during training
+    per_device_eval_batch_size=64,   # batch size for evaluation
+    warmup_steps=10,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.015,               # strength of weight decay
+    logging_dir='./logs',            # directory for storing logs
+    logging_steps=10,
+)
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions.argmax(-1)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='binary')
+    acc = accuracy_score(labels, preds)
+    return {
+        'accuracy': acc,
+        'f1': f1,
+        'precision': precision,
+        'recall': recall
+    }
+
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    #train_dataset=train_dataset,         # training dataset
+    eval_dataset=val_dataset,             # evaluation dataset
+    compute_metrics=compute_metrics
+)
+
+print(trainer.evaluate())
+
+# #############################################################################
+# END sanity check on the training set
+# #############################################################################
+
+# #############################################################################
+# Leak data testing
+# #############################################################################
+leak = pd.read_csv(base_dir+'socialmedia-disaster-tweets-DFE.csv', encoding='latin_1', keep_default_na=False)
+leak['target'] = (leak['choose_one']=='Relevant').astype(int)
+leak['id'] = leak.index
+leak = leak[['id', 'keyword','location','text', 'target']]
+print(leak.head(5))
+
+### test with leaked data ###
+leak["fulltext"] = leak["keyword"] + " " + leak["location"] + " " + leak["text"]
+final_test_encodings = tokenizer(leak["fulltext"].tolist(), truncation=True, padding=True)
+final_test_dataset = DisasterDataset(final_test_encodings, leak['target'].tolist())
+
+print(trainer.evaluate(final_test_dataset))
+
+# #############################################################################
+# END Leak data testing
+# #############################################################################
+
+
+exit(1)
+
+# #############################################################################
+# Create predictions
+# #############################################################################
 test=pd.read_csv(base_dir+'test.csv', keep_default_na=False)
+print(test.head(5))
 test["fulltext"] = test["keyword"] + " " + test["location"] + " " + test["text"]
 test_texts = test["fulltext"]
 
-
-from transformers import RobertaTokenizer, RobertaConfig, RobertaForSequenceClassification
-tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 test_encodings = tokenizer(test_texts.tolist(), truncation=True, padding=True)
+test_encodings = tokenizer(test_texts.tolist(), truncation=True, padding=True, return_tensors="pt")
+test_encodings.to(device)
 
-model = RobertaForSequenceClassification.from_pretrained("roberta-large")
-model.from_pretrained(base_dir + "v1model")
+print("Starting Predictions")
+with torch.no_grad():
+    preds = model(**test_encodings)
+print("Complete")
+print(preds)
 
-print()
-print()
-print(test_texts[0])
-inputs = tokenizer(test_texts[0], return_tensors="pt")
-outputs = model(**inputs)
-print(outputs.logits)
-print()
-print(test_texts[1])
-inputs = tokenizer(test_texts[1], return_tensors="pt")
-outputs = model(**inputs)
-print(outputs.logits)
-print()
-print(test_texts[2])
-inputs = tokenizer(test_texts[2], return_tensors="pt")
-outputs = model(**inputs)
-print(outputs.logits)
-print()
-print(test_texts[3])
-inputs = tokenizer(test_texts[2], return_tensors="pt")
-outputs = model(**inputs)
-print(outputs.logits)
-print()
-print(test_texts[4])
-inputs = tokenizer(test_texts[2], return_tensors="pt")
-outputs = model(**inputs)
-print(outputs.logits)
-print()
-print(test_texts[5])
-inputs = tokenizer(test_texts[2], return_tensors="pt")
-outputs = model(**inputs)
-print(outputs.logits)
+logits = preds.logits.detach().cpu().numpy()
+print(logits)
+pred_flat = np.argmax(logits, axis=1).flatten()
+
+#frame = { 'id': test['id'], 'target': pd.Series(pred_flat), 'logits': logits, 'fulltext': test["fulltext"] } 
+frame = { 'id': test['id'], 'target': pd.Series(pred_flat), 'fulltext': test["fulltext"] } 
+preds_out = pd.DataFrame(frame)
+print(preds_out.head(5))
+preds_out.to_csv('predictions.csv')
+
+
+submission = preds_out[['id', 'target']]
+print(submission.head(5))
+submission.to_csv('submission.csv',index=False)
